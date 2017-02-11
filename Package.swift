e8924675092241a29b275b3d01cb3e67364e2774
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-11 19:05:25 +0100

    Add perfect dependencies

--- Package.swift
+++ Package.swift
@@ -9,5 +9,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "VabeArchitecture"
+    name: "VabeArchitecture",
+    targets: [],
+    dependencies: [
+        .Package(
+            url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
+            versions: Version(0,0,0) ..< Version(10,0,0)
+        )
+    ],
+    exclude: []
 )
