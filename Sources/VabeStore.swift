4c774f3a83b5706c956e9b3f0104cdd2efdda963
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-13 23:59:08 +0100

    Add VabeStore

---
+++ Sources/VabeStore.swift
@@ -0,0 +1,17 @@
+//
+//  VabeStore.swift
+//  VabeArchitecture
+//
+//  Created by Valentin Bercot on 13/02/2017.
+//
+//
+
+public protocol VabeStore
+{
+    func count(identifiers: [String]?) throws -> Int
+    func list(identifers: [String]?, options: [VabeStoreOption]) throws -> [Any]?
+    func retrieve(identifiers: [String]) throws -> Any?
+    func create(identifers: [String]?, ressource: Any) throws -> Void
+    func update(identifiers: [String], ressource: Any) throws -> Void
+    func delete(identifiers: [String]) throws -> Void
+}
