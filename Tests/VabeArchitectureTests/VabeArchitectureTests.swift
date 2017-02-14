e538e1d68792509f5d7b0011f88e432152bfbaf7
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-14 23:56:23 +0100

    Remove main

--- Tests/VabeArchitectureTests/VabeArchitectureTests.swift
+++ Tests/VabeArchitectureTests/VabeArchitectureTests.swift
@@ -15,11 +15,11 @@ class VabeArchitectureTests: XCTestCase
     {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-
+        
         let text = "Hello, World!"
         XCTAssertEqual(text, "Hello, World!")
     }
-
+    
     static var allTests : [(String, (VabeArchitectureTests) -> () throws -> Void)]
     {
         return [
