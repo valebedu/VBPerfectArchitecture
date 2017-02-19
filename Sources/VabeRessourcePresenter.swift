a9ae9fc3ec6703c2bc664e9f252f84b5fd06291d
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-19 20:03:46 +0100

    Document presenters

--- Sources/VabeRessourcePresenter.swift
+++ Sources/VabeRessourcePresenter.swift
@@ -8,11 +8,57 @@
 
 import PerfectHTTP
 
+/**
+ Defines a `VabeRessourcePresenter` requirements. Extend it to create a presenter which present data for ressource fetched routes.
+ 
+ If you want to manage an executable route see `VabeExecutePresenter`.
+ 
+ - authors: Valentin Bercot
+ */
 public protocol VabeRessourcePresenter: VabePresenter
 {
+    /**
+     Present data in order to complete fetched list request.
+     
+     - parameters:
+       - ressources: ressources fetched from database.
+       - response: the HTTP response.
+     */
     func presentList(ressources: [Any]?, response: HTTPResponse)
+    
+    /**
+     Present data in order to complete fetched retrieve request.
+     
+     - parameters:
+       - ressource: ressource fetched from database.
+       - response: the HTTP response.
+     */
     func presentRetrieve(ressource: Any?, response: HTTPResponse)
+    
+    /**
+     Present data in order to complete fetched create request.
+     
+     - parameters:
+       - ressource: ressource fetched from database.
+       - response: the HTTP response.
+     */
     func presentCreate(ressource: Any?, response: HTTPResponse)
+    
+    /**
+     Present data in order to complete fetched update request.
+     
+     - parameters:
+       - ressource: ressource fetched from database.
+       - response: the HTTP response.
+     */
     func presentUpdate(ressource: Any?, response: HTTPResponse)
+    
+    /**
+     Present data in order to complete fetched delete request.
+     
+     - parameters:
+       - ressource: ressource fetched from database.
+       - response: the HTTP response.
+     */
     func presentDelete(ressource: Any?, response: HTTPResponse)
 }
