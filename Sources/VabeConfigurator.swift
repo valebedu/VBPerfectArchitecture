b0384f84028e0198643f4b7bc6d20543ce7b7136
Valentin Bercot <valent1.bercot@gmail.com>
2017-02-19 20:05:18 +0100

    Document configurator

--- Sources/VabeConfigurator.swift
+++ Sources/VabeConfigurator.swift
@@ -6,13 +6,27 @@
 //
 //
 
+/**
+ Defines a `VabeConfigurator` requirements. The configurator goal is to create controller interactor presenter cycle in order to complete VabeArchitecture.
+ 
+ - authors: Valentin Bercot
+ */
 public protocol VabeConfigurator
 {
+    /**
+     Congigure the controller interactor presenter cycle.
+     
+     - parameters:
+       - controller: the controller in which the cycle start.
+     */
     func configure(controller: VabeController)
 }
 
 public extension VabeConfigurator
 {
+    /**
+     Configurator shared instance.
+     */
     static var sharedInstance: Self {
         return self as! Self
     }
